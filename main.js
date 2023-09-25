7d480ee767abbb110b0610293812b25de21cf6c8
aleson pro <rolfaleson.pro@gmail.com>
2023-09-25 22:55:52 +0800

    feat add responsive navbar

---
+++ main.js
@@ -0,0 +1,5 @@
+// Toggle navigation links on mobile
+document.getElementById( "navbar-burger" ).addEventListener( "click", function () {
+    var navbarLinks = document.getElementById( "navbar-links" );
+    navbarLinks.classList.toggle( "active" );
+} );
