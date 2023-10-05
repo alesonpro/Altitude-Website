96df3e31e1da95f4ea072f8a4e890a357f1e8ec0
aleson pro <rolfaleson.pro@gmail.com>
2023-10-06 02:06:58 +0800

    feat updated website

--- main.js
+++
@@ -1,5 +0,0 @@
-// Toggle navigation links on mobile
-document.getElementById( "navbar-burger" ).addEventListener( "click", function () {
-    var navbarLinks = document.getElementById( "navbar-links" );
-    navbarLinks.classList.toggle( "active" );
-} );
